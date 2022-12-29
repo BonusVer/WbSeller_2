dd4d9fbe672a6b11d97dc14ef1ac7abdf5192c84
bonusver <minsvital@gmail.com>
2022-12-29 19:42:41 +0300

    Тест

---
+++ src/main/java/com/example/demo_01/ServiceCount.java
@@ -0,0 +1,94 @@
+package com.example.demo_01;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceCount {
+
+    public int[] sellerCore( int cost, int discount, int price) {
+        int temp[] = {0, 0};
+        float new_discount = 0f;
+        float cost_f = cost;
+        if (price < 50) {
+            System.out.println("Товару указана цена меньше 50 руб.");
+            temp[0] = cost;
+            temp[1] = discount;
+            return temp;
+        }
+        new_discount = 100 - (price * 100f / cost);
+        int n_discount = Math.round(new_discount);
+        float n_discount_f = (float) n_discount;
+
+        if (n_discount < 3 | n_discount > 95) {
+            if (n_discount < 3) {
+                float new_cost = price / (1 - 3f / 100);
+                int new_cost_f = (int) new_cost;
+                float delta = new_cost % 1;
+                if (delta > 0.001) new_cost_f = new_cost_f + 1;
+                int d_d = (int) (cost_f * 19 / 100);
+                int d_d_2 = cost + d_d;
+                if (new_cost_f <= d_d_2) {
+                    temp[0] = new_cost_f;
+                    temp[1] = 3;
+                    return temp;
+                } else {
+                    System.out.println("Цена товара не изменена. " +
+                            "Заявленная цена не соответствует правилам сайта. " +
+                            "Укажите ниже цену.");
+                    temp[0] = cost;
+                    temp[1] = discount;
+                    return temp;
+                }
+            }
+            if (n_discount > 95) {
+                float new_cost = price / (1 - 95f / 100);
+                int new_cost_f = (int) new_cost;
+                float delta = new_cost % 1;
+                if (delta > 0.001) new_cost_f = new_cost_f + 1;
+                int d_d = (int) (cost_f * 19 / 100);
+                int d_d_2 = cost - d_d;
+                if (new_cost_f >= d_d_2) {
+                    temp[0] = new_cost_f;
+                    temp[1] = 95;
+                    return temp;
+                } else {
+                    System.out.println("Цена до скидки была снижена более 20%");
+                    temp[0] = new_cost_f;
+                    temp[1] = 95;
+                    return temp;
+                }
+            }
+            return null;
+        } else if (n_discount > 60 & discount == 0) {
+            float new_cost = price / (1 - 60f / 100);
+            System.out.println(new_cost);
+            int new_cost_f = (int) new_cost;
+            float delta = new_cost % 1;
+            if (delta > 0.001) new_cost_f = new_cost_f + 1;
+            int d_d = (int) (cost_f * 20 / 100);
+            int d_d_2 = cost - d_d;
+            System.out.println(d_d_2);
+            if (new_cost_f >= d_d_2) {
+                temp[0] = new_cost_f;
+                temp[1] = 60;
+                return temp;
+            } else {
+                System.out.println("Скорее всего данный товар новый на сайте. " +
+                        "Скидка не может быть выше 60% по правилам маркетплейса. " +
+                        "Цена товара до скидки была снижена более 20% Если это не новый товар, то" +
+                        " поставьте в отчете скику, не равную 0");
+                temp[0] = new_cost_f;
+                temp[1] = 60;
+                return temp;
+            }
+        } else {
+            float new_cost = price / (1 - n_discount_f / 100);
+            int new_cost_f = (int) new_cost;
+            float delta = new_cost % 1;
+            if (delta > 0.001) new_cost_f = new_cost_f + 1;
+            temp[0] = new_cost_f;
+            temp[1] = n_discount;
+            return temp;
+        }
+    }
+}
--- src/main/java/com/example/demo_01/TestController.java
+++ src/main/java/com/example/demo_01/TestController.java
@@ -1,6 +1,11 @@
 package com.example.demo_01;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +15,10 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.*;
 
 @Controller
-public class TestController2 {
+public class TestController {
+
+    @Autowired
+    private ServiceCount serviceCount;
 
     @GetMapping("/")
     public String home() {
@@ -22,18 +30,48 @@ public class TestController2 {
             InputStream file01 = file[0].getInputStream();
             InputStream file02 = file[1].getInputStream();
 
-        XSSFWorkbook wb1 = new XSSFWorkbook(file01);
-        XSSFWorkbook wb2 = new XSSFWorkbook(file02);
+        XSSFWorkbook wb_price = new XSSFWorkbook(file01);
+        XSSFWorkbook wb_helper = new XSSFWorkbook(file02);
+        Sheet sheet_price = wb_price.getSheetAt(0);
+        Sheet sheet_helper = wb_helper.getSheetAt(0);
+
+        int z[] = {0,0};
+        for (Row row_helper: sheet_helper) {
+            Cell cell_helper = row_helper.getCell(0);
+            if (cell_helper.getCellType() == CellType.STRING) {
+                continue;
+            }
+            for (Row row_price : sheet_price) {
+                Cell cell_price = row_price.getCell(4);
+                if (cell_price.getCellType() == CellType.STRING) {
+                    continue;
+                } else {
+                    if (cell_helper.getNumericCellValue() == cell_price.getNumericCellValue()) {
+                        Cell cell_price_cost = row_price.getCell(11); // цена в отчете
+                        Cell cell_helper_sale = row_helper.getCell(1); //цена нужная
+                        Cell cell_price_now_discount = row_price.getCell(13);
+                        Cell cell_price_sale = row_price.getCell(15); //запись скидки
+                        Cell cell_price_price = row_price.getCell(12); // запись цены
+
+                        z = serviceCount.sellerCore((int) cell_price_cost.getNumericCellValue(),
+                                (int) cell_price_now_discount.getNumericCellValue(),
+                                (int) cell_helper_sale.getNumericCellValue());
+                        cell_price_sale.setCellValue(z[1]);
+                        cell_price_price.setCellValue(z[0]);
+
+                    }
+                }
+            }
+        }
+
         try {
             FileOutputStream outputStream = new FileOutputStream(
-                    new File("/Users/bonusver/Documents/XXXXXXXXXXXX.xlsx"));
-            FileOutputStream outputStream2 = new FileOutputStream(
-                    new File("/Users/bonusver/Documents/YYYYYYYYYYYY.xlsx"));
-            wb1.write(outputStream);
-            wb2.write(outputStream2);
+                    new File("/Users/bonusver/Desktop/Отчет отправка.xlsx"));
+            wb_price.write(outputStream);
         } catch (Exception e) {
             System.out.println("Exception: " + e);
         }
+
         return "download";
     }
 }
